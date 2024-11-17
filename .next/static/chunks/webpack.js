23f9004e1766785ada1bcb6443c9c7db76e79aac
Asim Hussain <asimhussain787@gmail.com>
2024-11-18 01:36:42 +0500

    Tags color changed

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cc8b51e1372a7677"; }
+/******/ 		__webpack_require__.h = function() { return "6ab05fc4985b58ee"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.770b6ac67d02a5e6.hot-update.js
+++ .next/static/webpack/webpack.770b6ac67d02a5e6.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "cc8b51e1372a7677"; }
+/******/ 	__webpack_require__.h = function() { return "6ab05fc4985b58ee"; }
 /******/ }();
 /******/ 
 /******/ }
