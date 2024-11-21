8aa5beab9a933a6022c4ef99c5870b2fd7df5698
Asim Hussain <asimhussain787@gmail.com>
2024-11-21 15:36:56 +0500

    new fixed

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 const About = () => {
   return (
     <div id='about' className='container pt-32'>
-        <h2 className='text-4xl md:text-5xl' data-aos="zoom-in-left">About Me</h2>
-        <p className='text-[#E5E5E5] pt-4' data-aos="zoom-in-left">As a student, I am currently expanding my expertise in Artificial Intelligence, Web 3.0, and the Metaverse through the GIAIC course. With a strong foundation in TypeScript, HTML, CSS, and JavaScript, I am passionate about creating innovative, user-centric websites and applications. </p>
+        <h2 className='text-4xl md:text-5xl' data-aos="fade-left">About Me</h2>
+        <p className='text-[#E5E5E5] pt-4' data-aos="fade-left">As a student, I am currently expanding my expertise in Artificial Intelligence, Web 3.0, and the Metaverse through the GIAIC course. With a strong foundation in TypeScript, HTML, CSS, and JavaScript, I am passionate about creating innovative, user-centric websites and applications. </p>
     </div>
   )
 }
