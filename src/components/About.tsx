aca1aaf92a4b9eb1f32ae7a9efca647e92d6447b
Asim Hussain <asimhussain787@gmail.com>
2024-11-21 15:10:22 +0500

    fix error

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div id='about' className='container pt-32'>
         <h2 className='text-4xl md:text-5xl' data-aos="zoom-in-left">About Me</h2>
-        <p className='text-[#E5E5E5] pt-4' data-aos="zoom-in-left">As a student, I'm currently expanding my expertise in Artificial Intelligence, Web 3.0, and the Metaverse through the GIAIC course. With a strong foundation in TypeScript, HTML, CSS, and JavaScript, I'm passionate about creating innovative, user-centric websites and applications.</p>
+        <p className='text-[#E5E5E5] pt-4' data-aos="zoom-in-left">As a student, I'm currently expanding my expertise in Artificial Intelligence, Web 3.0, and the Metaverse through the GIAIC course. With a strong foundation in TypeScript, HTML, CSS, and JavaScript, I'm passionate about creating innovative, user-centric websites and applications. {/* eslint-disable-line react/no-unescaped-entities */}</p>
     </div>
   )
 }
